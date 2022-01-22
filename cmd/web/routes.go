eb6eff3e848afc78964dde4ee04dd2ab4b428a22
Jareb Smith <jareb.smith@odmhsas.org>
2022-01-22 11:06:29 -0600

    Initial Commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/aceser199/bookings/internal/config"
+	"github.com/aceser199/bookings/internal/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	// middleware
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
